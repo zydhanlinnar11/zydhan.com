156a27d1145ad4a1bb31114325e6a1c70c1c3840
Zydhan Linnar Putra <zydhan.19051@mhs.its.ac.id>
2021-12-02 08:43:35 +0700

    Change back-end url

--- config/BlogConfig.ts
+++ config/BlogConfig.ts
@@ -3,6 +3,6 @@ import PostService from '../service/implementation/v3/PostService'
 const BLOG_TITLE = "Zydhan's Blog"
 const BLOG_DESC = 'Welcome to my blog. Have fun here!'
 const BLOG_DOMAIN = 'https://blog.zydhan.xyz'
-const BLOG_API = 'https://zydblog-api.000webhostapp.com/api'
+const BLOG_API = 'https://api.blog.zydhan.xyz'
 const POST_SERVICE = new PostService()
 export default { BLOG_TITLE, BLOG_DESC, BLOG_DOMAIN, BLOG_API, POST_SERVICE }
