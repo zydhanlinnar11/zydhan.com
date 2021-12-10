f04b8610817720a61dc0977005ff4c582e63ce00
Zydhan Linnar Putra <zydhan.19051@mhs.its.ac.id>
2021-12-10 22:36:08 +0700

    Add animation when blog post card hovered

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -12,14 +12,17 @@ export default function PostCard({ post, url }: Props) {
     <Link href={url} key={post.slug}>
       <a
         key={post.slug}
-        className='h-72 rounded-lg max-w-xs w-full mx-auto border border-white/[0.24]'
+        className='group h-72 rounded-xl max-w-xs w-full mx-auto border border-white/20 hover:border-white/50 duration-500'
       >
         <article className='h-full flex flex-col'>
-          <div className='h-32 block w-full relative'>
+          <div
+            className="h-32 block w-full relative rounded-t-xl overflow-hidden
+            after:content-[''] after:inline-block after:h-full after:w-full after:group-hover:bg-black/20 after:transition after:duration-500 after:ease-in-out"
+          >
             <Image
               src={post.coverUrl}
               alt={`${post.slug}-image`}
-              className='rounded-t-lg'
+              className='group-hover:scale-110 transition duration-500 ease-in-out rounded-t-xl -z-10'
               layout='fill'
               objectFit='cover'
               priority={true}
