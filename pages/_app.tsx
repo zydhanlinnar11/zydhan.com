5a744cc54edd0aded963ee9e329e73df964b52f0
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2021-12-21 23:36:17 +0700

    Fix few posts not full width

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }) {
     <div className='min-h-screen flex flex-col'>
       <AuthProvider>
         <Navbar />
-        <main className='flex flex-col mx-auto grow max-w-5xl px-6'>
+        <main className='flex flex-col mx-auto grow w-full max-w-5xl px-6'>
           <Component {...pageProps} />
         </main>
         <Footer />
