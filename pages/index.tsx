6e0132b22b22fc652582d66a735ad5a5404b2a1a
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2021-12-21 13:20:43 +0700

    Add fallback if there is no post available

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,11 +27,22 @@ export default function Home({ posts }: { posts: Post[] }) {
           user ? `Welcome, ${user.name}! Have fun here.` : BlogConfig.BLOG_DESC
         }
       />
-      <div className='text-center grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mx-auto gap-12 mb-14'>
-        {posts.map((post) => (
-          <PostCard post={post} url={`/post/${post.slug}`} key={post.slug} />
-        ))}
-      </div>
+      {posts.length > 0 ? (
+        <div className='text-center grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mx-auto gap-12 mb-14'>
+          {posts.map((post) => (
+            <PostCard post={post} url={`/post/${post.slug}`} key={post.slug} />
+          ))}
+        </div>
+      ) : (
+        <div className='text-center h-48 flex'>
+          <div className='m-auto'>
+            <h1 className='text-4xl'>Tidak ada pos</h1>
+            <p className='mt-3 text-gray-400'>
+              Saat ini belum ada pos, atau server sedang maintenance.
+            </p>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
