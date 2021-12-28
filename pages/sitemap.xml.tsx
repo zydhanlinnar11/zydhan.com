85e25a26094a620285659ae1481cf93e870f6c94
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2021-12-29 01:18:25 +0700

    Add sitemap and robots

---
+++ pages/sitemap.xml.tsx
@@ -0,0 +1,42 @@
+import { GetServerSideProps } from 'next'
+import BlogConfig from '../config/BlogConfig'
+import Post from '../models/Post'
+
+function generateSiteMap(posts: Post[]) {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+     <url>
+       <loc>${BlogConfig.BLOG_DOMAIN}</loc>
+     </url>
+     ${posts
+       .map(({ slug, updatedAt }) => {
+         return `
+       <url>
+           <loc>${`${BlogConfig.BLOG_DOMAIN}/post/${slug}`}</loc>
+           <lastmod>${updatedAt}</lastmod>
+       </url>
+     `
+       })
+       .join('')}
+   </urlset>
+ `
+}
+
+function SiteMap() {
+  // getServerSideProps will do the heavy lifting
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  const posts = await BlogConfig.POST_SERVICE.getAllPosts()
+  const sitemap = generateSiteMap(posts)
+
+  res.setHeader('Content-Type', 'text/xml')
+  res.write(sitemap)
+  res.end()
+
+  return {
+    props: {},
+  }
+}
+
+export default SiteMap
