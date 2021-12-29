66c6a68d29a5f3ec29b39aacd4c5fd499e550c23
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2021-12-30 00:46:37 +0700

    Remove manifest from document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -26,7 +26,6 @@ class MyDocument extends Document {
             sizes='180x180'
             href='/apple-touch-icon.png'
           ></link>
-          <link rel='manifest' href='manifest.json' />
           <meta name='theme-color' content='#111827' />
           <meta property='og:type' content='website' />
           <meta property='og:site_name' content="Zydhan's Blog" />
