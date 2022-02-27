1b6b928087c4a0880814b2633e1ee4b4b1fac156
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2022-02-28 05:05:32 +0700

    feat: add Button and TextInput elements

---
+++ src/common/components/elements/Button.tsx
@@ -0,0 +1,23 @@
+import { FC } from 'react'
+
+type Props = {
+  type?: 'button' | 'reset' | 'submit'
+  disabled?: any
+}
+
+const Button: FC<Props> = ({ children, type, disabled }) => {
+  return (
+    <button
+      type={type}
+      disabled={disabled}
+      className='rounded-md border-2 border-opacity-50 border-gray-600
+      w-full h-10 mt-3 hover:bg-blue-600 hover:bg-opacity-30 transition-colors
+      duration-100 focus:bg-blue-900 focus:bg-opacity-30
+      disabled:text-gray-400 disabled:hover:bg-transparent disabled:cursor-not-allowed'
+    >
+      {children}
+    </button>
+  )
+}
+
+export default Button
