a3dc3c513e24f49ad2b2f48aeee177d685ef0287
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2022-02-28 06:44:58 +0700

    feat: add onclick to button

--- src/common/components/elements/Button.tsx
+++ src/common/components/elements/Button.tsx
@@ -1,15 +1,17 @@
-import { FC } from 'react'
+import { FC, MouseEventHandler } from 'react'
 
 type Props = {
   type?: 'button' | 'reset' | 'submit'
-  disabled?: any
+  disabled?: boolean
+  onClick?: MouseEventHandler<HTMLButtonElement>
 }
 
-const Button: FC<Props> = ({ children, type, disabled }) => {
+const Button: FC<Props> = ({ children, type, disabled, onClick }) => {
   return (
     <button
       type={type}
       disabled={disabled}
+      onClick={onClick}
       className='rounded-md border-2 border-opacity-50 border-gray-600
       w-full h-10 mt-3 hover:bg-blue-600 hover:bg-opacity-30 transition-colors
       duration-100 focus:bg-blue-900 focus:bg-opacity-30
