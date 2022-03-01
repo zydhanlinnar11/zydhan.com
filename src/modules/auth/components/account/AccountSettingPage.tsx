39cc444e736b0aadc175e551f095ea2f231ed638
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2022-03-01 07:43:46 +0700

    feat(auth): ensure data being fetched before showing setting

--- src/modules/auth/components/account/AccountSettingPage.tsx
+++ src/modules/auth/components/account/AccountSettingPage.tsx
@@ -11,6 +11,8 @@ import useSWR from 'swr'
 import { faCircleUser } from '@fortawesome/free-regular-svg-icons'
 import TextInput from '@/common/components/elements/Form/TextInput'
 import { faGithub, faGoogle } from '@fortawesome/free-brands-svg-icons'
+import SpinnerLoading from '@/common/components/elements/SpinnerLoading'
+import Header from '@/common/components/elements/Header'
 
 type User = {
   name: string
@@ -33,226 +35,249 @@ const AccountSettingPage = () => {
   return (
     <PrivateRoute>
       <NarrowPageContainer>
-        <div className='w-full flex flex-col pb-10 gap-y-3 items-center sm:flex-row justify-between align-middle'>
-          <h2 className='text-2xl'>Account Setting</h2>
-          <div className='w-24 text-sm'>
-            <Button onClick={() => window.history.back()}>
-              <FontAwesomeIcon
-                className='my-auto mr-2'
-                icon={faCircleArrowLeft}
-              ></FontAwesomeIcon>{' '}
-              Back
-            </Button>
-          </div>
-        </div>
-        <div className='flex flex-col gap-y-5 sm:flex-row sm:gap-x-5'>
-          <div
-            className='sm:w-64 sm:h-fit bg-gray-900 flex flex-col w-full
+        {data && (
+          <>
+            <div className='w-full flex flex-col pb-10 gap-y-3 items-center sm:flex-row justify-between align-middle'>
+              <h2 className='text-2xl'>Account Setting</h2>
+              <div className='w-24 text-sm'>
+                <Button onClick={() => window.history.back()}>
+                  <FontAwesomeIcon
+                    className='my-auto mr-2'
+                    icon={faCircleArrowLeft}
+                  ></FontAwesomeIcon>{' '}
+                  Back
+                </Button>
+              </div>
+            </div>
+            <div className='flex flex-col gap-y-5 sm:flex-row sm:gap-x-5'>
+              <div
+                className='sm:w-64 sm:h-fit bg-gray-900 flex flex-col w-full
               border border-white/20 rounded p-6
               justify-center text-center gap-y-5'
-          >
-            {data && (
-              <>
-                <FontAwesomeIcon icon={faCircleUser} size={'4x'} />
-                <div>
-                  <p className='text-lg'>{data.name}</p>
-                  <p className='text-sm text-gray-400'>{data.email}</p>
+              >
+                <>
+                  <FontAwesomeIcon icon={faCircleUser} size={'4x'} />
+                  <div>
+                    <p className='text-lg'>{data.name}</p>
+                    <p className='text-sm text-gray-400'>{data.email}</p>
+                  </div>
+                </>
+              </div>
+
+              <div className='sm:w-full flex flex-col gap-y-5'>
+                <div
+                  className='flex flex-col
+              border border-white/20 rounded px-6 py-5'
+                >
+                  <h3 className='text-lg font-semibold'>Account Information</h3>
+                  <div className='h-px w-full bg-white/20 mt-3'></div>
+                  <form className='mt-5 flex flex-col gap-y-3'>
+                    <div className='grid grid-cols-3'>
+                      <div className='flex'>
+                        <label className='my-auto' htmlFor='name'>
+                          Name
+                        </label>
+                      </div>
+                      <TextInput
+                        className='col-span-2'
+                        label='Name'
+                        position='single'
+                        name='name'
+                        type={'text'}
+                        reference={null}
+                        autoComplete={'name'}
+                        defaultValue={data.name}
+                      />
+                    </div>
+
+                    <div className='grid grid-cols-3'>
+                      <div className='flex'>
+                        <label className='my-auto' htmlFor='email'>
+                          Email
+                        </label>
+                      </div>
+                      <TextInput
+                        className='col-span-2'
+                        label='Email'
+                        position='single'
+                        name='email'
+                        type={'text'}
+                        reference={null}
+                        autoComplete={'email'}
+                        defaultValue={data.email}
+                      />
+                    </div>
+
+                    <div className='mt-3 flex sm:justify-end'>
+                      <div className='w-full sm:w-1/3'>
+                        <Button>
+                          <span className='flex justify-center items-center gap-x-2'>
+                            <FontAwesomeIcon
+                              className='my-0'
+                              icon={faFloppyDisk}
+                            />{' '}
+                            Save
+                          </span>
+                        </Button>
+                      </div>
+                    </div>
+                  </form>
                 </div>
-              </>
-            )}
-          </div>
-
-          <div className='sm:w-full flex flex-col gap-y-5'>
-            <div
-              className='flex flex-col
+                <div
+                  className='flex flex-col
               border border-white/20 rounded px-6 py-5'
-            >
-              <h3 className='text-lg font-semibold'>Account Information</h3>
-              <div className='h-px w-full bg-white/20 mt-3'></div>
-              {data && (
-                <form className='mt-5 flex flex-col gap-y-3'>
-                  <div className='grid grid-cols-3'>
-                    <div className='flex'>
-                      <label className='my-auto' htmlFor='name'>
-                        Name
-                      </label>
+                >
+                  <h3 className='text-lg font-semibold'>Social Account</h3>
+                  <div className='h-px w-full bg-white/20 mt-3'></div>
+                  <div className='mt-5 flex flex-col gap-y-3'>
+                    <div className='grid grid-cols-3'>
+                      <p className='my-auto'>Google</p>
+                      <div className='col-span-2'>
+                        {data.linkedAccount.google ? (
+                          <Button>
+                            <span className='flex justify-center items-center gap-x-2'>
+                              <FontAwesomeIcon
+                                className='my-0'
+                                icon={faGoogle}
+                              />{' '}
+                              Unlink Google Account
+                            </span>
+                          </Button>
+                        ) : (
+                          <Button>
+                            <span className='flex justify-center items-center gap-x-2'>
+                              <FontAwesomeIcon
+                                className='my-0'
+                                icon={faGoogle}
+                              />{' '}
+                              Link Google Account
+                            </span>
+                          </Button>
+                        )}
+                      </div>
                     </div>
-                    <TextInput
-                      className='col-span-2'
-                      label='Name'
-                      position='single'
-                      name='name'
-                      type={'text'}
-                      reference={null}
-                      autoComplete={'name'}
-                      defaultValue={data.name}
-                    />
-                  </div>
 
-                  <div className='grid grid-cols-3'>
-                    <div className='flex'>
-                      <label className='my-auto' htmlFor='email'>
-                        Email
-                      </label>
-                    </div>
-                    <TextInput
-                      className='col-span-2'
-                      label='Email'
-                      position='single'
-                      name='email'
-                      type={'text'}
-                      reference={null}
-                      autoComplete={'email'}
-                      defaultValue={data.email}
-                    />
-                  </div>
-
-                  <div className='mt-3 flex sm:justify-end'>
-                    <div className='w-full sm:w-1/3'>
-                      <Button>
-                        <span className='flex justify-center items-center gap-x-2'>
-                          <FontAwesomeIcon
-                            className='my-0'
-                            icon={faFloppyDisk}
-                          />{' '}
-                          Save
-                        </span>
-                      </Button>
-                    </div>
-                  </div>
-                </form>
-              )}
-            </div>
-            <div
-              className='flex flex-col
-              border border-white/20 rounded px-6 py-5'
-            >
-              <h3 className='text-lg font-semibold'>Social Account</h3>
-              <div className='h-px w-full bg-white/20 mt-3'></div>
-              {data && (
-                <div className='mt-5 flex flex-col gap-y-3'>
-                  <div className='grid grid-cols-3'>
-                    <p className='my-auto'>Google</p>
-                    <div className='col-span-2'>
-                      {data.linkedAccount.google ? (
-                        <Button>
-                          <span className='flex justify-center items-center gap-x-2'>
-                            <FontAwesomeIcon className='my-0' icon={faGoogle} />{' '}
-                            Unlink Google Account
-                          </span>
-                        </Button>
-                      ) : (
-                        <Button>
-                          <span className='flex justify-center items-center gap-x-2'>
-                            <FontAwesomeIcon className='my-0' icon={faGoogle} />{' '}
-                            Link Google Account
-                          </span>
-                        </Button>
-                      )}
-                    </div>
-                  </div>
-
-                  <div className='grid grid-cols-3'>
-                    <p className='my-auto'>Github</p>
-                    <div className='col-span-2'>
-                      {data.linkedAccount.github ? (
-                        <Button>
-                          <span className='flex justify-center items-center gap-x-2'>
-                            <FontAwesomeIcon className='my-0' icon={faGithub} />{' '}
-                            Unlink Github Account
-                          </span>
-                        </Button>
-                      ) : (
-                        <Button>
-                          <span className='flex justify-center items-center gap-x-2'>
-                            <FontAwesomeIcon className='my-0' icon={faGithub} />{' '}
-                            Link Github Account
-                          </span>
-                        </Button>
-                      )}
+                    <div className='grid grid-cols-3'>
+                      <p className='my-auto'>Github</p>
+                      <div className='col-span-2'>
+                        {data.linkedAccount.github ? (
+                          <Button>
+                            <span className='flex justify-center items-center gap-x-2'>
+                              <FontAwesomeIcon
+                                className='my-0'
+                                icon={faGithub}
+                              />{' '}
+                              Unlink Github Account
+                            </span>
+                          </Button>
+                        ) : (
+                          <Button>
+                            <span className='flex justify-center items-center gap-x-2'>
+                              <FontAwesomeIcon
+                                className='my-0'
+                                icon={faGithub}
+                              />{' '}
+                              Link Github Account
+                            </span>
+                          </Button>
+                        )}
+                      </div>
                     </div>
                   </div>
                 </div>
-              )}
-            </div>
 
-            <div
-              className='sm:col-span-3 flex flex-col
+                <div
+                  className='sm:col-span-3 flex flex-col
               border border-white/20 rounded px-6 py-5'
-            >
-              <h3 className='text-lg font-semibold'>Change Password</h3>
-              <div className='h-px w-full bg-white/20 mt-3'></div>
-              {data && (
-                <form className='mt-5 flex flex-col gap-y-3'>
-                  <div className='grid grid-cols-3'>
-                    <div className='flex'>
-                      <label className='my-auto' htmlFor='current-password'>
-                        Current password
-                      </label>
+                >
+                  <h3 className='text-lg font-semibold'>Change Password</h3>
+                  <div className='h-px w-full bg-white/20 mt-3'></div>
+                  <form className='mt-5 flex flex-col gap-y-3'>
+                    <div className='grid grid-cols-3'>
+                      <div className='flex'>
+                        <label className='my-auto' htmlFor='current-password'>
+                          Current password
+                        </label>
+                      </div>
+                      <TextInput
+                        className='col-span-2'
+                        label='Current password'
+                        position='single'
+                        name='current-password'
+                        type={'password'}
+                        reference={null}
+                        autoComplete={'current-password'}
+                      />
                     </div>
-                    <TextInput
-                      className='col-span-2'
-                      label='Current password'
-                      position='single'
-                      name='current-password'
-                      type={'password'}
-                      reference={null}
-                      autoComplete={'current-password'}
-                    />
-                  </div>
 
-                  <div className='grid grid-cols-3'>
-                    <div className='flex'>
-                      <label className='my-auto' htmlFor='new-password'>
-                        New password
-                      </label>
+                    <div className='grid grid-cols-3'>
+                      <div className='flex'>
+                        <label className='my-auto' htmlFor='new-password'>
+                          New password
+                        </label>
+                      </div>
+                      <TextInput
+                        className='col-span-2'
+                        label='New password'
+                        position='single'
+                        name='new-password'
+                        type={'password'}
+                        reference={null}
+                        autoComplete={'new-password'}
+                      />
                     </div>
-                    <TextInput
-                      className='col-span-2'
-                      label='New password'
-                      position='single'
-                      name='new-password'
-                      type={'password'}
-                      reference={null}
-                      autoComplete={'new-password'}
-                    />
-                  </div>
 
-                  <div className='grid grid-cols-3'>
-                    <div className='flex'>
-                      <label className='my-auto' htmlFor='confirm-new-password'>
-                        Confirm password
-                      </label>
+                    <div className='grid grid-cols-3'>
+                      <div className='flex'>
+                        <label
+                          className='my-auto'
+                          htmlFor='confirm-new-password'
+                        >
+                          Confirm password
+                        </label>
+                      </div>
+                      <TextInput
+                        className='col-span-2'
+                        label='Confirm new password'
+                        position='single'
+                        name='confirm-new-password'
+                        type={'password'}
+                        reference={null}
+                        autoComplete={'new-password'}
+                      />
                     </div>
-                    <TextInput
-                      className='col-span-2'
-                      label='Confirm new password'
-                      position='single'
-                      name='confirm-new-password'
-                      type={'password'}
-                      reference={null}
-                      autoComplete={'new-password'}
-                    />
-                  </div>
 
-                  <div className='mt-3 flex sm:justify-end'>
-                    <div className='w-full sm:w-1/3'>
-                      <Button>
-                        <span className='flex justify-center items-center gap-x-2'>
-                          <FontAwesomeIcon
-                            className='my-0'
-                            icon={faFloppyDisk}
-                          />{' '}
-                          Change password
-                        </span>
-                      </Button>
+                    <div className='mt-3 flex sm:justify-end'>
+                      <div className='w-full sm:w-1/3'>
+                        <Button>
+                          <span className='flex justify-center items-center gap-x-2'>
+                            <FontAwesomeIcon
+                              className='my-0'
+                              icon={faFloppyDisk}
+                            />{' '}
+                            Change password
+                          </span>
+                        </Button>
+                      </div>
                     </div>
-                  </div>
-                </form>
-              )}
+                  </form>
+                </div>
+              </div>
             </div>
+          </>
+        )}
+        {!data && !error && (
+          <div className='my-auto'>
+            <SpinnerLoading />
           </div>
-        </div>
+        )}
+        {error && (
+          <Header
+            className='my-auto'
+            midText='An error ocurred'
+            bottomText='Please try again later.'
+          />
+        )}
       </NarrowPageContainer>
     </PrivateRoute>
   )
