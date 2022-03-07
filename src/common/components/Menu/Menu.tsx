7b58c472a90118f1ab51cd46f664e0f26aac214f
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2022-03-08 03:34:19 +0700

    refactor: make menu as a component

---
+++ src/common/components/Menu/Menu.tsx
@@ -0,0 +1,43 @@
+import { FC } from 'react'
+import { Menu as HeadlessMenu, Transition } from '@headlessui/react'
+import { Fragment } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { IconProp, SizeProp } from '@fortawesome/fontawesome-svg-core'
+
+type Props = {
+  icon: IconProp
+  iconSize: SizeProp
+}
+
+const Menu: FC<Props> = ({ children, icon, iconSize }) => {
+  return (
+    <HeadlessMenu as='div' className='ml-3 relative'>
+      <div>
+        <HeadlessMenu.Button className='bg-gray-800 flex text-sm rounded-full'>
+          <span className='sr-only'>Open user menu</span>
+          <span className='text-gray-300'>
+            <FontAwesomeIcon icon={icon} size={iconSize} />
+          </span>
+        </HeadlessMenu.Button>
+      </div>
+      <Transition
+        as={Fragment}
+        enter='transition ease-out duration-100'
+        enterFrom='transform opacity-0 scale-95'
+        enterTo='transform opacity-100 scale-100'
+        leave='transition ease-in duration-75'
+        leaveFrom='transform opacity-100 scale-100'
+        leaveTo='transform opacity-0 scale-95'
+      >
+        <HeadlessMenu.Items
+          className='absolute origin-top-right bg-gray-900 flex
+              flex-col right-0 w-56 mt-2 border border-white/20 rounded py-1 z-10 break-all'
+        >
+          {children}
+        </HeadlessMenu.Items>
+      </Transition>
+    </HeadlessMenu>
+  )
+}
+
+export default Menu
--- src/common/components/Navbar/UserMenu.tsx
+++ src/common/components/Navbar/UserMenu.tsx
@@ -1,16 +1,15 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { Menu, Transition } from '@headlessui/react'
-import { Fragment } from 'react'
 import { faCircleUser } from '@fortawesome/free-regular-svg-icons'
 import {
   faArrowRightFromBracket,
   faGear,
 } from '@fortawesome/free-solid-svg-icons'
-import MenuItem from './MenuItem'
 import { useUserDispatch, useUserState } from '@/common/providers/UserProvider'
 import Router from 'next/router'
 import logout from '@/modules/auth/utils/Logout'
 import { toast } from 'react-toastify'
+import Menu from '@/components/Menu'
+import MenuItem from '@/components/Menu/MenuItem'
 
 const UserMenu = () => {
   const userState = useUserState()
@@ -32,76 +31,51 @@ const UserMenu = () => {
 
   return (
     <ul className='flex gap-x-8'>
-      <Menu as='div' className='ml-3 relative'>
-        <div>
-          <Menu.Button className='bg-gray-800 flex text-sm rounded-full'>
-            <span className='sr-only'>Open user menu</span>
-            <span className='text-gray-300'>
+      <Menu icon={faCircleUser} iconSize='2x'>
+        {userState.state === 'loading' && (
+          <>
+            <div className='flex flex-col gap-y-3 items-center justify-center py-3 px-6'>
               <FontAwesomeIcon icon={faCircleUser} size={'2x'} />
-            </span>
-          </Menu.Button>
-        </div>
-        <Transition
-          as={Fragment}
-          enter='transition ease-out duration-100'
-          enterFrom='transform opacity-0 scale-95'
-          enterTo='transform opacity-100 scale-100'
-          leave='transition ease-in duration-75'
-          leaveFrom='transform opacity-100 scale-100'
-          leaveTo='transform opacity-0 scale-95'
-        >
-          <Menu.Items
-            className='absolute origin-top-right bg-gray-900 flex
-                  flex-col right-0 w-56 mt-2 border border-white/20 rounded py-1 z-10 break-all'
-          >
-            {userState.state === 'loading' && (
-              <>
-                <div className='flex flex-col gap-y-3 items-center justify-center py-3 px-6'>
-                  <FontAwesomeIcon icon={faCircleUser} size={'2x'} />
-                  <div className='text-center flex flex-col gap-y-1 w-full'>
-                    <div className='h-2 bg-slate-700 rounded animate-pulse'></div>
-                    <div className='h-2 mx-5 bg-slate-700 rounded animate-pulse'></div>
-                  </div>
-                </div>
-                <div className='bg-gray-600 h-px mx-4 my-1' />
-              </>
-            )}
-            {userState.state === 'authenticated' && (
-              <>
-                <div className='flex flex-col gap-y-3 items-center justify-center py-3 px-6'>
-                  <FontAwesomeIcon icon={faCircleUser} size={'2x'} />
-                  <div className='text-center'>
-                    <p>{userState.user.name}</p>
-                    <small className='text-gray-400'>
-                      {userState.user.email}
-                    </small>
-                  </div>
-                </div>
-                <div className='bg-gray-600 h-px mx-4 my-1' />
-                <MenuItem onClick={() => Router.push('/auth/account')}>
-                  <FontAwesomeIcon icon={faGear} className='mr-2 my-0' />
-                  Setting
-                </MenuItem>
-                <MenuItem onClick={logoutHandler}>
-                  <FontAwesomeIcon
-                    icon={faArrowRightFromBracket}
-                    className='mr-2 my-0'
-                  />
-                  Sign out
-                </MenuItem>
-              </>
-            )}
-            {userState.state === 'unauthenticated' && (
-              <MenuItem onClick={() => Router.push('/auth/login')}>
-                <FontAwesomeIcon
-                  icon={faArrowRightFromBracket}
-                  className='mr-2 my-0'
-                />
-                Sign in
-              </MenuItem>
-            )}
-          </Menu.Items>
-        </Transition>
+              <div className='text-center flex flex-col gap-y-1 w-full'>
+                <div className='h-2 bg-slate-700 rounded animate-pulse'></div>
+                <div className='h-2 mx-5 bg-slate-700 rounded animate-pulse'></div>
+              </div>
+            </div>
+            <div className='bg-gray-600 h-px mx-4 my-1' />
+          </>
+        )}
+        {userState.state === 'authenticated' && (
+          <>
+            <div className='flex flex-col gap-y-3 items-center justify-center py-3 px-6'>
+              <FontAwesomeIcon icon={faCircleUser} size={'2x'} />
+              <div className='text-center'>
+                <p>{userState.user.name}</p>
+                <small className='text-gray-400'>{userState.user.email}</small>
+              </div>
+            </div>
+            <div className='bg-gray-600 h-px mx-4 my-1' />
+            <MenuItem onClick={() => Router.push('/auth/account')}>
+              <FontAwesomeIcon icon={faGear} className='mr-2 my-0' />
+              Setting
+            </MenuItem>
+            <MenuItem onClick={logoutHandler}>
+              <FontAwesomeIcon
+                icon={faArrowRightFromBracket}
+                className='mr-2 my-0'
+              />
+              Sign out
+            </MenuItem>
+          </>
+        )}
+        {userState.state === 'unauthenticated' && (
+          <MenuItem onClick={() => Router.push('/auth/login')}>
+            <FontAwesomeIcon
+              icon={faArrowRightFromBracket}
+              className='mr-2 my-0'
+            />
+            Sign in
+          </MenuItem>
+        )}
       </Menu>
     </ul>
   )
