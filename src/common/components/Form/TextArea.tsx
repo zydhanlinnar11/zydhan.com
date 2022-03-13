18308be56ee065e49f8318bd36781669f4e9f107
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2022-03-13 15:55:58 +0700

    feat: textarea component

---
+++ src/common/components/Form/TextArea.tsx
@@ -0,0 +1,20 @@
+import { forwardRef, InputHTMLAttributes } from 'react'
+
+const TextArea = forwardRef<
+  HTMLTextAreaElement,
+  InputHTMLAttributes<HTMLTextAreaElement>
+>((props, ref) => {
+  const { className } = props
+
+  return (
+    <textarea
+      {...props}
+      ref={ref}
+      className={`block focus:ring-4 focus:ring-blue-600 focus:ring-opacity-30 focus:outline-none w-full px-4 py-2 rounded-md bg-transparent border border-white/[0.24] ${className}`}
+    />
+  )
+})
+
+TextArea.displayName = 'TextArea'
+
+export default TextArea
