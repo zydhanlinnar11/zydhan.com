d65362a27d6441401395a6a2971ddfc3ae111e92
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2022-03-13 14:55:44 +0700

    refactor: add custom href to post card

--- src/modules/blog/components/PostCard.tsx
+++ src/modules/blog/components/PostCard.tsx
@@ -5,16 +5,17 @@ import { Post } from './PostListPage'
 
 type Props = {
   post: Post
+  href: string
 }
 
-const PostCard: FC<Props> = ({ post }) => {
+const PostCard: FC<Props> = ({ post, href }) => {
   return (
-    <Link href={`/blog/posts/${post.slug}`}>
+    <Link href={href}>
       <a
         key={post.slug}
-        className='group h-72 rounded-xl max-w-xs w-full mx-auto border border-white/20 hover:border-white/50 duration-500'
+        className="group h-72 rounded-xl max-w-xs w-full mx-auto border border-white/20 hover:border-white/50 duration-500"
       >
-        <article className='h-full flex flex-col'>
+        <article className="h-full flex flex-col">
           <div
             className="h-32 block w-full relative rounded-t-xl overflow-hidden
   after:content-[''] after:inline-block after:h-full after:w-full after:group-hover:bg-black/20 after:transition after:duration-500 after:ease-in-out"
@@ -22,18 +23,18 @@ const PostCard: FC<Props> = ({ post }) => {
             <Image
               src={post.cover_url}
               alt={`${post.slug}-image`}
-              className='group-hover:scale-110 transition duration-500 ease-in-out rounded-t-xl -z-10'
-              layout='fill'
-              objectFit='cover'
+              className="group-hover:scale-110 transition duration-500 ease-in-out rounded-t-xl -z-10"
+              layout="fill"
+              objectFit="cover"
               priority={true}
-              sizes='320px'
+              sizes="320px"
             />
           </div>
-          <div className='text-left p-6 flex flex-col justify-between flex-auto'>
+          <div className="text-left p-6 flex flex-col justify-between flex-auto">
             <div>
-              <h3 className='font-bold text-lg'>{post.title}</h3>
+              <h3 className="font-bold text-lg">{post.title}</h3>
             </div>
-            <p className='text-gray-400 font-semibold text-sm'>
+            <p className="text-gray-400 font-semibold text-sm">
               {post.created_at}
             </p>
           </div>
