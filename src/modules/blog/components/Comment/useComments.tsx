9b87d24671f459477ceae7142335d220e523a3ca
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2022-03-14 13:27:29 +0700

    feat(blog): send credentials when fetch comment

--- src/modules/blog/components/Comment/useComments.tsx
+++ src/modules/blog/components/Comment/useComments.tsx
@@ -1,8 +1,8 @@
-import axios from 'axios'
 import useSWR from 'swr'
 import Comment from '@/modules/blog/types/admin/Comment'
+import { axiosAPI } from '@/common/utils/AxiosInstance'
 
-const fetcher = (url: string) => axios.get(url).then((res) => res.data)
+const fetcher = (url: string) => axiosAPI.get(url).then((res) => res.data)
 
 const useComments = (postSlug: string) => {
   const { data, error, mutate } = useSWR<Comment[]>(
