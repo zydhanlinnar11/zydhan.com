9d413a6f32d698367ee3bfdc5e653bbf1fb3af88
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2022-03-15 12:09:07 +0700

    feat(portfolio): make language card smaller

--- src/modules/portfolio/components/LangAndTools/Card.tsx
+++ src/modules/portfolio/components/LangAndTools/Card.tsx
@@ -8,9 +8,9 @@ type Props = {
 
 const Card: FC<Props> = ({ name, image }) => {
   return (
-    <div className="w-40 h-40 bg-white/20 rounded flex flex-col justify-center hover:scale-105 duration-200">
-      <div className="pb-6 flex justify-center">
-        <Image src={image} height={50} width={50} priority={true} />
+    <div className="w-32 h-32 bg-white/20 rounded flex flex-col justify-between p-6 hover:scale-105 duration-200">
+      <div className="flex justify-center">
+        <Image src={image} height={48} width={48} priority={true} />
       </div>
       <p className="font-medium text-center">{name}</p>
     </div>
