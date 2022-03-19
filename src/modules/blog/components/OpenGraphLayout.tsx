c2473fb6333a6539c426e8a6dfe5aed661e1e784
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2022-03-19 22:06:40 +0700

    fix: unreadable og font

--- src/modules/blog/components/OpenGraphLayout.tsx
+++ src/modules/blog/components/OpenGraphLayout.tsx
@@ -17,7 +17,7 @@ const OpenGraphLayout: FC<Props> = ({ title, description, date, userName }) => {
   return (
     <div
       id="opengraph"
-      className="w-[1200px] h-[600px] bg-black text-white flex flex-col justify-between pt-16"
+      className="w-[1200px] h-[600px] bg-black text-white flex flex-col justify-between pt-12"
     >
       <div className="flex flex-col justify-center items-center text-center px-16">
         <div>
@@ -29,11 +29,11 @@ const OpenGraphLayout: FC<Props> = ({ title, description, date, userName }) => {
             alt="Animated photo of Zydhan Linnar Putra"
           />
         </div>
-        <p className="mt-8 text-5xl font-medium">{title}</p>
-        <p className="mt-6 text-2xl text-gray-400">{description}</p>
+        <p className="mt-8 text-6xl font-medium">{title}</p>
+        <p className="mt-6 text-3xl text-gray-400">{description}</p>
       </div>
       <div>
-        <div className="grid grid-cols-3 mb-4 text-gray-300 justify-items-center text-lg">
+        <div className="grid grid-cols-3 mb-4 text-gray-300 justify-items-center text-2xl">
           <p>
             <FontAwesomeIcon className="my-auto mr-2" icon={faUser} />
             <span> {userName}</span>
