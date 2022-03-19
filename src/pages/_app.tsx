b5f8d747687ca5942ed4116352f4b6451cae3301
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2022-03-19 19:11:25 +0700

    feat: update meta description

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,18 +10,21 @@ import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
 
+const description = `Hello friends!, you can call me Zydhan. I'm web development enthusiast with ${
+  new Date().getFullYear() - 2021
+} year of experience${
+  new Date().getFullYear() > 2022 ? ' s' : ''
+} and currently study to become Bachelor of Informatics Engineering at Sepuluh Nopember Institute of Technology Surabaya.`
+
 const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
   return (
     <>
       <Head>
         <title>Zydhan Linnar Putra</title>
-        <meta name="description" content="Welcome to My Personal Portfolio" />
+        <meta name="description" content={description} />
         <meta property="og:title" content="Zydhan Linnar Putra" />
         <meta property="og:url" content="https://zydhan.xyz" />
-        <meta
-          property="og:description"
-          content="Welcome to Zydhan Linnar Putra Personal Website"
-        />
+        <meta property="og:description" content={description} />
         <meta name="theme-color" content="#111827" />
         <meta property="og:type" content="website" />
         <meta property="og:site_name" content="Zydhan Linnar Putra" />
