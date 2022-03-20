28fe7b4588cd7bda3785ea565778c522081e55a3
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2022-03-20 16:10:34 +0700

    Revert "feat: cache opengraph image to firebase db"
    This reverts commit 31453a53c3b5e1c3f48d1d49d6e58d1515473407.

--- src/pages/api/og-image/blog-post/index.ts
+++ src/pages/api/og-image/blog-post/index.ts
@@ -1,96 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import chromium from 'chrome-aws-lambda'
 import config from '@/common/config'
-import db from '@/common/utils/db'
-
-type BlogOpengraph = {
-  data: string
-  date: string
-  description: string
-  title: string
-  url: string
-  userName: string
-}
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const title = req.query.title
-  const description = req.query.description
-  const date = req.query.date
-  if (
-    typeof title !== 'string' ||
-    typeof description !== 'string' ||
-    typeof date !== 'string'
-  ) {
-    res.status(400).json({ message: 'Not found' })
-    return
-  }
-
-  const entries = await db
-    .collection('blog_opengraphs')
-    .where('title', '==', title)
-    .where('description', '==', description)
-    .where('date', '==', date)
-    .limit(1)
-    .get()
-
-  const entriesData = entries.docs.map((entry) => entry.data() as BlogOpengraph)
-
-  if (entriesData.length !== 0) {
-    const data = entriesData[0].data
-    const img = Buffer.from(
-      data.replace('data:image/png;base64,', ''),
-      'base64'
-    )
-    res
-      .setHeader('Content-Type', 'image/png')
-      .setHeader('Content-Length', img.length)
-      .setHeader(
-        'Cache-Control',
-        `immutable, no-transform, s-max-age=2592000, max-age=2592000`
-      )
-      .send(img)
-    return
-  }
-
-  try {
-    const image = await generateImage(title, description, date)
-    if (!(image instanceof Buffer)) throw new Error('image is not a buffer')
-    res
-      .setHeader('Content-Type', 'image/png')
-      .setHeader(
-        'Cache-Control',
-        `immutable, no-transform, s-max-age=2592000, max-age=2592000`
-      )
-      .send(image)
-
-    const data = 'data:image/png;base64,' + image.toString('base64')
-    const cached: BlogOpengraph = {
-      data,
-      date,
-      description,
-      title,
-      url: config.baseUrl + '/blog',
-      userName: 'Zydhan Linnar Putra',
-    }
-    await db.collection('blog_opengraphs').add(cached)
-  } catch (e) {
-    console.error(e)
-    res.status(500).send({ message: 'Internal server error' })
-  }
-}
-
-const generateImage = async (
-  title: string,
-  description: string,
-  date: string
-) => {
-  const queryString = new URLSearchParams()
-  queryString.append('title', title)
-  queryString.append('description', description)
-  queryString.append('date', date)
+  const queryString = Object.keys(req.query)
+    .map((key) => key + '=' + req.query[key])
+    .join('&')
 
   const browser = await chromium.puppeteer.launch({
     args: [...chromium.args, '--hide-scrollbars', '--disable-web-security'],
@@ -100,9 +18,7 @@ const generateImage = async (
     ignoreHTTPSErrors: true,
   })
   const page = await browser.newPage()
-  await page.goto(
-    `${config.baseUrl}/og-image/blog-post?${queryString.toString()}`
-  )
+  await page.goto(`${config.baseUrl}/og-image/blog-post?${queryString}`)
 
   await page.evaluate(async () => {
     const selectors = Array.from(document.querySelectorAll('img'))
@@ -127,12 +43,18 @@ const generateImage = async (
 
   const opengraph = await page.$('#opengraph')
   if (!opengraph) {
-    throw Error('cant capture opengraph')
+    res.status(500).send({ message: 'Internal server error' })
     return
   }
 
   const image = await opengraph.screenshot({ omitBackground: true })
   await browser.close()
 
-  return image
+  res
+    .setHeader('Content-Type', 'image/png')
+    .setHeader(
+      'Cache-Control',
+      `immutable, no-transform, s-max-age=2592000, max-age=2592000`
+    )
+    .send(image)
 }
