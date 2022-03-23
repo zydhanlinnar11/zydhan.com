c506645e8bcd53231bbbbe5ca9be241154cfb520
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2022-03-24 01:13:53 +0700

    fix(apps): fetching before getting user

--- src/modules/apps/components/AuthorizationPage/AuthorizationPage.tsx
+++ src/modules/apps/components/AuthorizationPage/AuthorizationPage.tsx
@@ -1,4 +1,5 @@
 import FullscreenLoading from '@/common/components/FullscreenLoading'
+import { useUserState } from '@/common/providers/UserProvider'
 import { axiosAPI } from '@/common/utils/AxiosInstance'
 import PrivateRoute from '@/modules/auth/components/PrivateRoute'
 import axios from 'axios'
@@ -7,12 +8,16 @@ import React, { useEffect } from 'react'
 
 const AuthorizationPage = () => {
   const router = useRouter()
+  const userState = useUserState()
 
   useEffect(() => {
     if (!router.isReady) return
     const response_type = router.query.response_type
     const app_id = router.query.app_id
     const redirect_uri = router.query.redirect_uri
+
+    if (userState.state !== 'authenticated') return
+
     if (
       typeof app_id !== 'string' ||
       typeof redirect_uri !== 'string' ||
@@ -34,7 +39,7 @@ const AuthorizationPage = () => {
           router.replace('/500')
         router.replace('/400')
       })
-  }, [router.isReady])
+  }, [router.isReady, userState.state])
 
   return (
     <PrivateRoute>
