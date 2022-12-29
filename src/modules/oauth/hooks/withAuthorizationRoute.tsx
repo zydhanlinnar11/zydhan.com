a60d6dbfd80423df72d99288aa733e2e5cc36d33
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2022-12-30 03:04:05 +0700

    refactor: handle oauth login prompt

--- src/modules/oauth/hooks/withAuthorizationRoute.tsx
+++ src/modules/oauth/hooks/withAuthorizationRoute.tsx
@@ -1,7 +1,8 @@
 import { useRouter } from 'next/router'
-import { useUser } from '@/common/providers/UserProvider'
+import { useRefetchUser, useUser } from '@/common/providers/UserProvider'
 import { convertQueryToSearchParams } from '@/common/tools/ConvertQueryToSearchParams'
 import { ComponentType, FC, PropsWithChildren } from 'react'
+import { backendFetcher } from '@/common/hooks/useAxios'
 
 const withAuthorizationRoute = <P,>(Component: ComponentType<P>) => {
   const ComponentWithProps = (props: P) => (
@@ -16,6 +17,7 @@ const withAuthorizationRoute = <P,>(Component: ComponentType<P>) => {
 const AuthorizationRoute: FC<PropsWithChildren> = ({ children }) => {
   const { state } = useUser()
   const router = useRouter()
+  const refetchUser = useRefetchUser()
 
   if (state === 'UNAUTHENTICATED') {
     const params = convertQueryToSearchParams(router.query)
@@ -24,6 +26,14 @@ const AuthorizationRoute: FC<PropsWithChildren> = ({ children }) => {
     router.push(`/auth/login?${params.toString()}`)
   }
   if (state === 'LOADING' || state === 'UNAUTHENTICATED') return <p>Loading</p>
+
+  if (router.query.prompt === 'login') {
+    delete router.query.prompt
+    backendFetcher.delete('/auth/logout').then(() => {
+      refetchUser && refetchUser()
+    })
+    return <></>
+  }
 
   return <>{children}</>
 }
