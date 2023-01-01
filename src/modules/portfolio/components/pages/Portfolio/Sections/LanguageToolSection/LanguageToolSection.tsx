cffa22b2bec428a6b574caeb6a3d4bb70d16c2fc
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2023-01-01 22:12:30 +0700

    fix: nextjs logo in light mode

--- src/modules/portfolio/components/pages/Portfolio/Sections/LanguageToolSection/LanguageToolSection.tsx
+++ src/modules/portfolio/components/pages/Portfolio/Sections/LanguageToolSection/LanguageToolSection.tsx
@@ -3,20 +3,26 @@ import Section from '../Section'
 import LanguageTool from './LanguageTool'
 import laravel from './images/laravel.png'
 import nextjs from './images/nextjs.png'
+import nextjsBlack from './images/nextjs-black.png'
 import typescript from './images/typescript.png'
 import php from './images/php.png'
 import docker from './images/docker.png'
-import { Flex } from '@chakra-ui/react'
-
-const languageTools: LanguageToolType[] = [
-  { image: laravel, name: 'Laravel' },
-  { image: nextjs, name: 'Next.js' },
-  { image: typescript, name: 'TypeScript' },
-  { image: php, name: 'PHP' },
-  { image: docker, name: 'Docker' },
-]
+import { Flex, useColorMode } from '@chakra-ui/react'
+import { useMemo } from 'react'
 
 const LanguageToolSection = () => {
+  const { colorMode } = useColorMode()
+  const languageTools: LanguageToolType[] = useMemo(
+    () => [
+      { image: laravel, name: 'Laravel' },
+      { image: colorMode === 'dark' ? nextjs : nextjsBlack, name: 'Next.js' },
+      { image: typescript, name: 'TypeScript' },
+      { image: php, name: 'PHP' },
+      { image: docker, name: 'Docker' },
+    ],
+    [colorMode]
+  )
+
   return (
     <Section title="Favorite Language and Tools">
       <Flex wrap={'wrap'} gap={4} w={'full'} justifyContent={'center'}>
