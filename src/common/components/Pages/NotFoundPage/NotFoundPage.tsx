98091661de71b3d2ad8901fdf9106bba70257761
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2023-02-09 01:18:03 +0700

    feat: add error page title

--- src/common/components/Pages/NotFoundPage/NotFoundPage.tsx
+++ src/common/components/Pages/NotFoundPage/NotFoundPage.tsx
@@ -1,19 +1,23 @@
 import { Button, Flex, Heading, Text } from '@chakra-ui/react'
+import { NextSeo } from 'next-seo'
 import Link from 'next/link'
 import NotFoundIcon from './NotFoundIcon'
 
 const NotFoundPage = () => {
   return (
-    <Flex my={'auto'} textAlign={'center'} flexDir={'column'} gap={'4'}>
-      <Flex marginX={'auto'}>
-        <NotFoundIcon />
+    <>
+      <NextSeo title="Page Not Found" />
+      <Flex my={'auto'} textAlign={'center'} flexDir={'column'} gap={'4'}>
+        <Flex marginX={'auto'}>
+          <NotFoundIcon />
+        </Flex>
+        <Heading as={'h1'}>404 - Page Not Found</Heading>
+        <Text>Sorry, we couldn&apos;t find what you&apos;re looking for.</Text>
+        <Link href={'/'}>
+          <Button>Back to Home Page</Button>
+        </Link>
       </Flex>
-      <Heading as={'h1'}>404 - Page Not Found</Heading>
-      <Text>Sorry, we couldn&apos;t find what you&apos;re looking for.</Text>
-      <Link href={'/'}>
-        <Button>Back to Home Page</Button>
-      </Link>
-    </Flex>
+    </>
   )
 }
 
