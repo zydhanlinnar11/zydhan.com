90105a31e096b59df0d3b16e5791441e9326c8fb
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2023-02-08 20:09:18 +0700

    chore: basecontroller accomodate ssr response

--- src/common/backend/controllers/BaseController.ts
+++ src/common/backend/controllers/BaseController.ts
@@ -1,16 +1,66 @@
+import { IncomingMessage, ServerResponse } from 'http'
 import { NextApiResponse } from 'next'
 export abstract class BaseController {
-  static badRequest = (res: NextApiResponse) =>
-    res.status(400).send({ message: 'Bad Request!' })
+  static isNextApiResponse = function (
+    res: NextApiResponse | ServerResponse<IncomingMessage>
+  ): res is NextApiResponse {
+    return 'status' in res
+  }
 
-  static notFound = (res: NextApiResponse) =>
-    res.status(404).send({ message: 'Not Found!' })
+  static writeJson = (
+    res: ServerResponse<IncomingMessage>,
+    data: any,
+    statusCode: number = 200
+  ) => {
+    res.setHeader('Content-Type', 'application/json')
+    res.statusCode = statusCode
+    res.write(JSON.stringify(data))
+    res.end()
+  }
 
-  static unauthorized = (res: NextApiResponse) =>
-    res.status(401).send({ message: 'Unauthorized!' })
+  static badRequest = (
+    res: NextApiResponse | ServerResponse<IncomingMessage>
+  ) => {
+    const data = { message: 'Bad Request!' }
+    if (this.isNextApiResponse(res))
+      return res.status(400).send({ message: 'Bad Request!' })
 
-  static methodNotAllowed = (res: NextApiResponse) =>
-    res.status(405).send({ message: 'Method not allowed!' })
+    this.writeJson(res, data, 400)
+  }
 
-  static noContent = (res: NextApiResponse) => res.status(204).send(null)
+  static notFound = (
+    res: NextApiResponse | ServerResponse<IncomingMessage>
+  ) => {
+    const data = { message: 'Not Found!' }
+    if (this.isNextApiResponse(res)) return res.status(404).send(data)
+
+    this.writeJson(res, data, 404)
+  }
+
+  static unauthorized = (
+    res: NextApiResponse | ServerResponse<IncomingMessage>
+  ) => {
+    const data = { message: 'Unauthorized!' }
+    if (this.isNextApiResponse(res)) return res.status(401).send(data)
+
+    this.writeJson(res, data, 401)
+  }
+
+  static methodNotAllowed = (
+    res: NextApiResponse | ServerResponse<IncomingMessage>
+  ) => {
+    const data = { message: 'Method not allowed!' }
+    if (this.isNextApiResponse(res)) return res.status(405).send(data)
+
+    this.writeJson(res, data, 405)
+  }
+
+  static noContent = (
+    res: NextApiResponse | ServerResponse<IncomingMessage>
+  ) => {
+    const data = null
+    if (this.isNextApiResponse(res)) return res.status(204).send(data)
+
+    this.writeJson(res, data, 204)
+  }
 }
