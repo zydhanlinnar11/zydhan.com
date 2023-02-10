d75743a7d56bfa32b4fdb6788c1a7fcab96d6498
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2023-02-11 05:20:14 +0700

    feat: redirect intended and error handling in login page

--- src/modules/auth/components/pages/LoginPage/LoginPage.tsx
+++ src/modules/auth/components/pages/LoginPage/LoginPage.tsx
@@ -1,17 +1,26 @@
 import useSocialMediaList from '@/auth/hooks/useSocialMediaList'
 import LoadingPage from '@/common/components/Pages/LoadingPage'
+import ServerErrorPage from '@/common/components/Pages/ServerErrorPage'
 import { config } from '@/common/config'
 import { Container, Heading, Text } from '@chakra-ui/react'
 import { useSession } from 'next-auth/react'
 import { NextSeo } from 'next-seo'
+import { useRouter } from 'next/router'
 import { memo } from 'react'
 import SocialMediaLoginButton from '../../Button/SocialMediaLoginButton'
 
 const LoginPage = () => {
   const { error, isLoading, socialMedia } = useSocialMediaList()
   const { status } = useSession()
+  const { isReady, push } = useRouter()
 
-  if (status === 'authenticated' || status === 'loading') return <LoadingPage />
+  if (status === 'authenticated' && isReady) {
+    push('/')
+  }
+
+  if (status === 'authenticated' || status === 'loading' || isLoading)
+    return <LoadingPage />
+  if (error) return <ServerErrorPage />
 
   return (
     <>
