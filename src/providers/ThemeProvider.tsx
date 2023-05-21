c37d9a89323e595d1ff02543df0e97314621c4fc
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2023-05-21 17:00:29 +0700

    feat: add theme mode persistence mechanism

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -29,10 +29,51 @@ const initialTheme: Theme = {
 
 // Create the context for the theme
 const ThemeContext = createContext<Theme>(initialTheme)
+const getSystemPreferredTheme = () => {
+  // Check if the system prefers a light theme
+  if (
+    window.matchMedia &&
+    window.matchMedia('(prefers-color-scheme: light)').matches
+  ) {
+    return 'light'
+  }
+
+  // Check if the system prefers a dark theme
+  if (
+    window.matchMedia &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches
+  ) {
+    return 'dark'
+  }
+
+  // Return null if the system preference is not available or not supported
+  return null
+}
+
+const updateTheme = (theme: Theme, newTheme: Partial<Theme>) => {
+  const htmlElement = document.documentElement
+
+  // Remove existing theme class names
+  Object.values(themes).forEach((theme) => {
+    Object.values(theme).forEach((className) => {
+      if (!className) return
+      htmlElement.classList.remove(className)
+    })
+  })
+
+  // Add new theme class name
+  const { color, mode } = { ...theme, ...newTheme }
+  const themeStyles = themes[color]
+  if (themeStyles && themeStyles[mode]) {
+    htmlElement.classList.add(themeStyles[mode])
+  }
+}
 
 // Create the ThemeProvider component
 const ThemeProvider = ({ children }: PropsWithChildren) => {
   const [theme, setTheme] = useState(initialTheme)
+  console.log(theme)
+
   useEffect(() => {
     const handleClassNameChange = () => {
       const htmlElement = document.documentElement
@@ -68,32 +109,37 @@ const ThemeProvider = ({ children }: PropsWithChildren) => {
     }
   }, [])
 
+  useEffect(() => {
+    // Set the theme mode from localStorage
+    const persistedMode = localStorage.getItem('themeMode')
+    if (persistedMode) {
+      updateTheme(theme, { mode: persistedMode as 'light' | 'dark' })
+    } else {
+      // If theme.mode is undefined, check system's preferred theme
+      if (!theme.mode) {
+        const systemPreferredTheme = getSystemPreferredTheme()
+        updateTheme(theme, {
+          mode: systemPreferredTheme || 'dark',
+        })
+      }
+    }
+  }, [])
+
+  useEffect(() => {
+    // Update localStorage when theme mode changes
+    localStorage.setItem('themeMode', theme.mode)
+  }, [theme.mode])
+
   return <ThemeContext.Provider value={theme}>{children}</ThemeContext.Provider>
 }
 
 const useTheme = () => {
   const theme = useContext(ThemeContext)
 
-  const updateTheme = (newTheme: Partial<Theme>) => {
-    const htmlElement = document.documentElement
-
-    // Remove existing theme class names
-    Object.values(themes).forEach((theme) => {
-      Object.values(theme).forEach((className) => {
-        if (!className) return
-        htmlElement.classList.remove(className)
-      })
-    })
-
-    // Add new theme class name
-    const { color, mode } = { ...theme, ...newTheme }
-    const themeStyles = themes[color]
-    if (themeStyles && themeStyles[mode]) {
-      htmlElement.classList.add(themeStyles[mode])
-    }
+  return {
+    theme,
+    setTheme: (newTheme: Partial<Theme>) => updateTheme(theme, newTheme),
   }
-
-  return { theme, setTheme: updateTheme }
 }
 
 export { ThemeProvider, useTheme }
