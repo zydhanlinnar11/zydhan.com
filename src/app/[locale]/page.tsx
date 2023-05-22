01a6a15780c3129be38d87e94e7155a1bead28fd
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2023-05-22 12:29:28 +0700

    feat: make view better for expanded screen

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import siteLogo from '@/assets/images/site-logo.webp'
 import typography from '@/styles/typography.module.scss'
 import { useTranslations } from 'next-intl'
+import clsx from 'clsx'
 
 export default function Home() {
   const t = useTranslations('Home')
@@ -18,9 +19,25 @@ export default function Home() {
             height={64}
             alt={t('header.logo_alt')}
           />
-          <h1 className={typography['headline-medium']}>{t('header.title')}</h1>
-          <p>{t('header.subtitle')}</p>
-          <p className={typography['body-small']}>{t('header.description')}</p>
+          <h1
+            className={clsx(
+              typography['headline-medium'],
+              typography['screen-medium-headline-large']
+            )}
+          >
+            {t('header.title')}
+          </h1>
+          <p className={typography['screen-medium-body-large']}>
+            {t('header.subtitle')}
+          </p>
+          <p
+            className={clsx(
+              typography['body-small'],
+              typography['screen-medium-body-medium']
+            )}
+          >
+            {t('header.description')}
+          </p>
         </header>
 
         <main></main>
