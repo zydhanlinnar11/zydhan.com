eb7c24cf2426838a35df8b980d07a70cda16a8b8
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2023-08-02 16:51:31 +0700

    chore: install chakra ui

---
+++ src/app/providers.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { CacheProvider } from '@chakra-ui/next-js'
+import { ChakraProvider, ColorModeScript, extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({})
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <CacheProvider>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <ChakraProvider theme={theme}>{children}</ChakraProvider>
+    </CacheProvider>
+  )
+}
