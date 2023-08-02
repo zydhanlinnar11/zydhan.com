8edc0874940ac641f050a2c5ce9c6abcab9cd556
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2023-08-02 18:23:32 +0700

    feat: fully functional navbar

--- src/app/[locale]/providers.tsx
+++ src/app/[locale]/providers.tsx
@@ -1,14 +1,13 @@
 'use client'
 
 import { CacheProvider } from '@chakra-ui/next-js'
-import { ChakraProvider, ColorModeScript, extendTheme } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 
 export const theme = extendTheme({})
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <CacheProvider>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <ChakraProvider theme={theme}>{children}</ChakraProvider>
     </CacheProvider>
   )
