a5f02406fa6708dbc5a8491d0734f8d7cc88c3d1
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2023-08-06 13:50:34 +0700

    refactor: mdx structuring and frontmatter

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -1,26 +1,31 @@
 import { ProjectMetadata } from '@/types/ProjectMetadata'
 import { readdirSync } from 'fs'
-import matter from 'gray-matter'
+
+const basePath = './src/app/[locale]/projects'
 
 export const getProjectsMetadata = async (locale: 'en' | 'id') => {
-  const basePath = './src/app/[locale]/projects'
-  const projects = readdirSync(basePath, {
-    withFileTypes: true,
-  })
-    .filter((dirent) => dirent.isDirectory())
-    .map((dirent) => dirent.name)
+  const slugs = readdirSync(basePath)
+    .filter((name) => name.endsWith('.mdx'))
+    .map((name) => name.replace('.mdx', ''))
 
-  const projectsMetadata: ProjectMetadata[] = []
-  for (let i = 0; i < projects.length; i++) {
-    const project = projects[i]
-    const mdxFilePath = `${basePath}/${project}/page.mdx`
-    const metadata = matter.read(mdxFilePath).data
-    projectsMetadata.push({
-      ...metadata,
-      slug: project,
-      subtitle: metadata.subtitle[locale],
-    } as ProjectMetadata)
+  const metadatas: ProjectMetadata[] = []
+  for (let i = 0; i < slugs.length; i++) {
+    const slug = slugs[i]
+    metadatas.push(await readProjectMetadata(slug, locale))
   }
 
-  return projectsMetadata
+  return metadatas
+}
+
+export const readProjectMetadata = async (
+  slug: string,
+  locale: 'en' | 'id'
+) => {
+  const metadata = (await import(`@/app/[locale]/projects/${slug}.mdx`)).meta
+
+  return {
+    ...metadata,
+    slug,
+    subtitle: metadata.subtitle[locale],
+  } as ProjectMetadata
 }
